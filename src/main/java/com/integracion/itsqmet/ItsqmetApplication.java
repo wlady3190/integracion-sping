6e0484af364d5e1f092efd7e6af672bd7fe58c31
wlady3190 <wlady_3190@hotmail.com>
2024-02-01 08:59:58 -0500

    creacion de estructura del proyecto

---
+++ src/main/java/com/integracion/itsqmet/ItsqmetApplication.java
@@ -0,0 +1,13 @@
+package com.integracion.itsqmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsqmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsqmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integracion/itsqmet/ItsqmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integracion.itsqmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsqmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
